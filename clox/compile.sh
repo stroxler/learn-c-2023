d78b430c2a01c893edb8fbc97dcceaa78b111a1c
Steven Troxler <steven.troxler@gmail.com>
2023-05-14 09:23:06 -0700

    Initial working compile on macos

---
+++ clox/compile.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Compile clox by hand on a macos machine
+
+# Notes:
+# - The -c flag on gcc causes it to ouptut an object file
+# - The -L and -lSystem flags are required on macos to find
+#   the system library.
+
+export CFLAGS="-c"
+
+gcc -c -o main.o main.c
+
+ld \
+   -macos_version_min 13.3.1 -arch arm64 \
+   -L$(xcode-select -p)/SDKs/MacOSX.sdk/usr/lib -lSystem \
+   -o clox.exe \
+   main.o
