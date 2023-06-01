3c9167941451fdc2ce4a7adc8bf4eef6b8fd0d2a
Steven Troxler <steven.troxler@gmail.com>
2023-06-01 09:11:20 -0700

    LCTHW ex2

---
+++ lcthw/ex2/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Compile this by and with `gcc ex1.c`, then run it with `./a.out`.
+
+int main(int args, char *argv[])
+{
+  int distance = 100;
+  printf("You are %d miles away.\n", distance);
+  return 0;
+}
